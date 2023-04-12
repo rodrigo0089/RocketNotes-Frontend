c22d17a96e36d45e27202d1f87c0f8020dabfdfd
Rodrigo Faria <rodrigo0089@hotmail.com>
2023-04-12 20:29:13 -0300

    connection api with frontend is ok

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -2,7 +2,7 @@ import { useState  } from "react";
 import { FiMail, FiLock, FiUser } from "react-icons/fi";
 import { Link, useNavigate } from "react-router-dom";
 
-import { api } from '../../services/api'
+import { api } from '../../services/api';
 
 import { Input } from "../../components/Input";
 import { Button } from "../../components/Button";
