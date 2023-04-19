96af719fcdad27257259f02c5cfd3fbb7f61552f
Rodrigo Faria <rodrigo0089@hotmail.com>
2023-04-19 19:16:20 -0300

    add hook auth

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
+import { AuthProvider } from './hooks/auth';
 
 import GlobalStyles from './styles/global';
 
@@ -12,8 +13,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme ={theme}>
       <GlobalStyles />
-      <Routes />
+      <AuthProvider>
+        <Routes />
+      </AuthProvider>
     </ThemeProvider>
-    
-  </React.StrictMode>,
+    </React.StrictMode>,
 )
